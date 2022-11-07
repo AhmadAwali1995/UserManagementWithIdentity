b3f8418e5972691ed19e345bbabe5971b189efc3
Ahmad Awaly <aawali73@yahoo.com>
2022-11-07 23:00:59 +0200

    Add project files.

---
+++ UserManagementWithIdentity/Controllers/RolesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagementWithIdentity.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(string RoleName)
+        {
+            if(!ModelState.IsValid)
+                return View("Index", await _roleManager.Roles.ToListAsync());
+
+            if (await _roleManager.RoleExistsAsync(RoleName))
+            {
+                ModelState.AddModelError("Name", "Role is exists!");
+                return View("Index", await _roleManager.Roles.ToListAsync());
+            }
+
+            await _roleManager.CreateAsync(new IdentityRole(RoleName.Trim()));
+
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
